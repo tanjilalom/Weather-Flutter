f4d2c054fcb11012db9da6c29a7a85ad93004c81
tanjilalom <tanjil15-3079@diu.edu.bd>
2024-05-02 17:32:50 +0600

    Initial Commit

--- lib/service/weather_service.dart
+++ lib/service/weather_service.dart
@@ -1,7 +1,5 @@
 import 'dart:convert';
 import 'package:get/get.dart';
-
-import '../model/weather_model.dart';
 import 'package:http/http.dart' as http;
 import 'package:geolocator/geolocator.dart';
 
